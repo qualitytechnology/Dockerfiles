f5d9045f972ff558ad087d14d1714b025ff1f61a
Tibor Vukovic <tiborvukovic91@gmail.com>
2018-12-05 16:59:47 +0100

    Use aws cli to get kubeconfig

--- aws-kubectl/entrypoint.sh
+++ aws-kubectl/entrypoint.sh
@@ -2,9 +2,6 @@
 set -e
 
 aws configure set default.region ${CLUSTER_REGION}
-export CA_CERT=$(aws eks describe-cluster --name ${CLUSTER_NAME}  --query cluster.certificateAuthority.data)
-export ENDPOINT_URL=$(aws eks describe-cluster --name ${CLUSTER_NAME}  --query cluster.endpoint)
-cat /config | envsubst > /kube/config
-export KUBECONFIG=/kube/config
+aws eks update-kubeconfig --name ${CLUSTER_NAME}
 
 exec "$@"
